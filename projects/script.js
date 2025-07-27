29b51f169f94b61e0d5ff188465ca0943b1b86d2
shahil8848 <shahilbhusal2002@gmail.com>
2025-07-28 03:58:56 +0545

    Hide 'View All' button on mobile view using CSS media query

--- projects/script.js
+++ projects/script.js
@@ -1,88 +1,97 @@
 $(document).ready(function () {
 
-    // Menu toggle for mobile hamburger
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
 
-    // On scroll or load, reset menu and toggle scroll-top button
     $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
 
         if (window.scrollY > 60) {
-            $('#scroll-top').addClass('active');
+            document.querySelector('#scroll-top').classList.add('active');
         } else {
-            $('#scroll-top').removeClass('active');
+            document.querySelector('#scroll-top').classList.remove('active');
         }
     });
 });
 
-// Change title and favicon on tab visibility change
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState === "visible") {
-        document.title = "Projects | Portfolio Shahil Bhusal";
-        $("#favicon").attr("href", "/assets/images/logo.png");
-    } else {
-        document.title = "The journey continues";
-        $("#favicon").attr("href", "/assets/images/cmsoon.png");
-    }
-});
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio Shahil Bhusal";
+            $("#favicon").attr("href", "/assets/images/logo.png");
+        }
+        else {
+            document.title = "The journey continues";
+            $("#favicon").attr("href", "/assets/images/cmsoon.png");
+        }
+    });
 
-// Fetch projects JSON data
+
+// fetch projects start
 function getProjects() {
     return fetch("projects.json")
-        .then(response => response.json());
+        .then(response => response.json())
+        .then(data => {
+            return data
+        });
 }
 
-// Render projects and initialize Isotope and VanillaTilt
+
 function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
-
     projects.forEach(project => {
         projectsHTML += `
-        <div class="grid-item">
-          <div class="box tilt">
-            <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-            <div class="content">
-              <div class="tag">
-                <h3>${project.name}</h3>
-              </div>
-              <div class="desc">
-                <p>${project.desc}</p>
-                <div class="btns">
-                  <a href="${project.links.view}" class="btn" target="_blank">
-                    <i class="fas fa-eye"></i> View
-                  </a>
-                  <a href="${project.links.code}" class="btn" target="_blank">
-                    Code <i class="fas fa-code"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
+        <div class="grid-item ${project}">
+        <div class="box tilt" style="width: 380px; margin: 1rem">
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
           </div>
-        </div>`;
+        </div>
+      </div>
+    </div>
+    </div>`
     });
-
     projectsContainer.innerHTML = projectsHTML;
 
-    // Initialize VanillaTilt
-    VanillaTilt.init(document.querySelectorAll(".tilt"), {
-        max: 15,
-        speed: 400,
-        glare: true,
-        "max-glare": 0.2,
-    });
+    // vanilla tilt.js
+    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    //     max: 20,
+    // });
+    // // vanilla tilt.js  
 
-    // Initialize Isotope
+    // /* ===== SCROLL REVEAL ANIMATION ===== */
+    // const srtop = ScrollReveal({
+    //     origin: 'bottom',
+    //     distance: '80px',
+    //     duration: 1000,
+    //     reset: true
+    // });
+
+    // /* SCROLL PROJECTS */
+    // srtop.reveal('.work .box', { interval: 200 });
+
+    // isotope filter products
     var $grid = $('.box-container').isotope({
         itemSelector: '.grid-item',
-        layoutMode: 'fitRows'
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
     });
 
-    // Filter buttons (if any)
+    // filter items on button click
     $('.button-group').on('click', 'button', function () {
         $('.button-group').find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
@@ -91,26 +100,28 @@ function showProjects(projects) {
     });
 }
 
-// Call fetch and render projects
 getProjects().then(data => {
     showProjects(data);
-});
+})
+// fetch projects end
 
-// Disable developer mode keys
+
+
+// disable developer mode
 document.onkeydown = function (e) {
-    if (e.keyCode === 123) { // F12
+    if (e.keyCode == 123) {
         return false;
     }
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
         return false;
     }
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
         return false;
     }
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
         return false;
     }
-    if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
         return false;
     }
-};
+}
